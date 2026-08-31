5b6dffb7165dd14166822b153d591eeff2c356ca
AYESHA <dazyash0@gmail.com>
2026-09-01 01:29:09 +0500

    featured 70%

--- tour_and_travel/src/Components/FeaturedDestination.jsx
+++ tour_and_travel/src/Components/FeaturedDestination.jsx
@@ -10,9 +10,13 @@ import back from '../assets/three.jfif'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Clock } from 'lucide-react'
 
 const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
-  <img src={ } aly='prevArrow' {...props} />
+  <img src={next} alt='prevArrow' {...props} />
+);
+const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+  <img src={back} alt='prevArrow' {...props} />
 );
 
 
@@ -24,8 +28,39 @@ const FeaturedDestination = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3
-    nextArrow:
+    slidesToScroll: 3,
+    nextArrow: <SlickArrowLeft />,
+    prevArrow: <SlickArrowRight />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+
+
+
+    ]
+
   };
 
   const destinationJson = [
@@ -39,12 +74,72 @@ const FeaturedDestination = () => {
 
   return (
     <>
-    <section className='w-full py-12 md:py-24 lg:pt-32 px-6 md:px-0'>
-      <div className ='max-w-7xl mx-auto px-4 md:px-6'>
-        <h2>Featured Destinations</h2>
-      </div>
+      <section className='w-full py-12 md:py-24 lg:pt-32 px-6 md:px-0'>
+        <div className='max-w-7xl mx-auto px-4 md:px-6'>
+          <h2 className='text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-3 font-serif'>Featured Destinations</h2>
+          <hr className='text-red-500 w-[200px] bg-red-500 mx-auto h-1 mb-10' />
+          <div className="slider-container">
+            <Slider {...settings}>
+              {destinationJson.map((destination) => {
+                <div>
+                  <div key ={destination.name} className = ' overflow-hidden border shadow-lg shadow-gray-500 rounded-lg mb-5 mr-5'>
+                    <div className =''>
+                      <img
+                      src={destination.img}
+                      alt ={destination.name}
+                      width={600}
+                      height={400}
+                      className='object-cover w-full h-48 hover:scale-110 transition-all'
 
-    </section>
+                      />
+                      <div clasName='p-4'>
+                        <p classNAme = 'text-gray-500 flex items-center gap-1 text-sm mb-1 '><Clock width={15}/>{destination.time}</p>
+                        <h3 className = 'text-xl font-bold mb-2'>{destination.name}</h3>
+<p className = 'flex gap-1 items-center '><Star width={20}/>{destination.star}</p>
+                      </div>
+
+                    </div>
+
+                  </div>
+                </div>
+
+
+              })}
+
+
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+              <div>
+                <h3>6</h3>
+              </div>
+              <div>
+                <h3>7</h3>
+              </div>
+              <div>
+                <h3>8</h3>
+              </div>
+              <div>
+                <h3>9</h3>
+              </div>
+            </Slider>
+          </div>
+
+        </div>
+
+      </section>
 
 
 
